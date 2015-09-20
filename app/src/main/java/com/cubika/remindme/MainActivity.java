1d6980ee2dc5b31f6582eaddbf73c58c3a0b51ea
bulkozavr <tom.smith.2013@mail.ru>
2015-09-20 15:09:34 +0300

    Created MainActivity class and layout

---
+++ app/src/main/java/com/cubika/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cubika.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Rumata on 20.09.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
